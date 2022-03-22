3bd7ea726422e77eea82a153770d8a4420cac521
MarekOcz <kitty29@talktalk.net>
2022-03-22 21:44:55 +0000

    Fixed the __init__ statement in the ImprovedNumber object when the error was caused by the automatic 'e' in the normal float object

--- BaseMath.py
+++ BaseMath.py
@@ -1,3 +1,6 @@
+from typing import *
+
+
 class MathError(Exception):
     pass
 
@@ -27,13 +30,12 @@ class ImprovedNumber:
         """
         if type(num) not in (int, float):
             raise TypeError("parameter 'num' needs to be of type 'int' or 'float'.")
-        if num != int(num):
-            x = len(str(num).split(".")[1])
-            self.multiplier = x
-            self.int_num = int(num * (10 ** x))
-        else:
-            self.multiplier = 0
-            self.int_num = int(num)
+        self.multiplier = 0
+        self.int_num = num
+        while int(self.int_num) != self.int_num:
+            self.int_num *= 10
+            self.multiplier += 1
+        self.int_num = int(self.int_num)
 
     def standard_form(self, sig_figs: int = 3, unicode_superscript: bool = True) -> str:
         """
@@ -42,11 +44,25 @@ class ImprovedNumber:
         :param unicode_superscript: How do you want the code to be returned?
         :return: The standard form of the value as a string format. May add a standard form class
         """
-        if self.multiplier > 0:
+        if len(str(self.int_num)) < self.multiplier:  # Negative power of 10
+            ten_power = len(str(self.int_num)) - self.multiplier
             if unicode_superscript:
-                return str(self.int_num)[0] + "." + str(self.int_num)[1:sig_figs] + "×10⁻" + "".join(
-                    ImprovedNumber.superscripts[i] for i in str(self.multiplier))
+                return str(self.int_num)[0] + "." + str(self.int_num)[1:sig_figs] + "×10" + "".join(
+                    ImprovedNumber.superscripts[i] for i in str(ten_power))
+
+
+def random_func_gen(length: int, func: Callable) -> str:
+    from random import randint
+    returned = ""
+    for i in range(length):
+        temp = randint(0, 1000) / (10 ** randint(0, 10))
+        print(str(temp) + " " * (17 - len(str(temp))) + "      ", end="")
+        temp_class = ImprovedNumber(temp)
+        returned += str(temp) + " " * (17 - len(str(temp)))
+        print(str(func(temp_class)) + "\n")
+        returned += str(func(temp_class)) + "\n"
+    return returned
 
 
 if __name__ == '__main__':
-    pass
+    print(random_func_gen(length=1000, func=lambda x: ImprovedNumber.standard_form(self=x)))
